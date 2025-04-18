cdce6215984e99cf61b40b19d83ee0173085ac43
Omar-elmetwalli <164086888+Omar-elmetwalli@users.noreply.github.com>
2025-04-19 00:04:43 +0200

    Add files via upload

---
+++ backend/Admin/Create_Users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages.Admin
+{
+    public class Create_UsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ backend/Admin/Stats.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages.Admin
+{
+    public class StatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
