fb741cf3e0516a86b085532ff48b518c97aaa0c8
RaedFrase <167563020+RaedFrase@users.noreply.github.com>
2025-04-20 14:09:05 +0200

    .

--- WebCode/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ WebCode/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111605e4481574ef97e53ceb35bb6dc4c980c7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
