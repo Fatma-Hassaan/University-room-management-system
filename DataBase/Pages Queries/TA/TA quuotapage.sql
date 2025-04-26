6eea81f0b17c66026a7020589eae517d2d2c621f
Omar-elmetwalli <164086888+Omar-elmetwalli@users.noreply.github.com>
2025-04-26 04:20:03 +0300

    Add files via upload

---
+++ DataBase/Pages Queries/TA/TA quuotapage.sql
@@ -0,0 +1,25 @@
+
+SELECT Quota
+FROM   Student
+WHERE  ID = @StudentID;
+
+
+DECLARE @RID            INT            =  ,
+        @ExpectedDelivery Date            =  , 
+		@UserID INT =                            ,
+		@RoomID INT = ,
+		@ExtraHours INT = ;
+
+
+
+
+INSERT INTO RequestOrReport
+      (UserID,  Condition,  DayofR,         HourofR,        RType)
+VALUES(@UserID,'Pending',CAST(GETDATE() AS date), CAST(GETDATE() AS time), 'AdditionalQuotaRequest');
+
+SET @RID = SCOPE_IDENTITY();
+
+
+INSERT INTO AdditionalQuotaRequest (ID, RoomID, NumOfExtraHours)
+VALUES (@RID, @RoomID, @ExtraHours);
+
