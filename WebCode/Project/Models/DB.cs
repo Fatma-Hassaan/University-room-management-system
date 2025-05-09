5f59f8b0b0dcc1e9e8060bc2732b0741eabfb6b7
RaedFrase <167563020+RaedFrase@users.noreply.github.com>
2025-05-09 14:11:53 +0300

    .

--- WebCode/Project/Models/DB.cs
+++ WebCode/Project/Models/DB.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Drawing;
 using Microsoft.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Project.Models
 {
@@ -11,36 +12,37 @@ namespace Project.Models
 
         private SqlConnection con { get; set; }
 
-        public DB() { 
-            con=new SqlConnection(ConnectionString);
+        public DB()
+        {
+            con = new SqlConnection(ConnectionString);
         }
 
         public string GetUserType(string Email)
         {
-            string UserType="";
+            string UserType = "";
 
             string query = "SELECT UserType FROM [User] WHERE Email = @Email\r\nUNION\r\nSELECT UserType FROM [Admin] WHERE Email =@Email";
             SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@Email",Email);
+            cmd.Parameters.AddWithValue("@Email", Email);
             try
             {
                 con.Open();
-                UserType = (string) cmd.ExecuteScalar(); 
+                UserType = (string)cmd.ExecuteScalar();
             }
             catch (Exception ex)
             {
-                
+
             }
-            finally 
-            { 
-                con.Close(); 
+            finally
+            {
+                con.Close();
             }
             return UserType;
         }
 
         public int GetUserID(string Email)
         {
-            int ID=0;
+            int ID = 0;
 
             string query = "SELECT UserID FROM [User] WHERE Email = @Email\r\nUNION\r\nSELECT ID FROM [Admin] WHERE Email =@Email";
             SqlCommand cmd = new SqlCommand(query, con);
@@ -63,8 +65,8 @@ namespace Project.Models
 
         public bool CheckPassword(int ID, string Password)
         {
-            bool match=false;
-            string p="";
+            bool match = false;
+            string p = "";
             string query = "SELECT [Password] FROM [User] WHERE UserID =@ID\r\nUNION\r\nSELECT [Password] FROM [Admin] WHERE ID =@ID";
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.Parameters.AddWithValue("@ID", ID);
@@ -144,7 +146,7 @@ namespace Project.Models
             if (UserType == "") query = "Select UserID, UserType, [Name], Email From [User] Where [Name] like '%' + @Name + '%'";
             else if (Name == "") query = "Select UserID, UserType, [Name], Email From [User] Where [UserType] like '%' + @UserType + '%'";
             else if (Name != "" && UserType != "") query = "Select UserID, UserType, [Name], Email From [User] Where [UserType] like '%' + @UserType + '%' And [Name] like '%' + @Name + '%'";
-            else query= "Select UserID, UserType, [Name], Email From [User]";
+            else query = "Select UserID, UserType, [Name], Email From [User]";
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.Parameters.AddWithValue("@UserType", UserType);
             cmd.Parameters.AddWithValue("@Name", Name);
@@ -165,15 +167,16 @@ namespace Project.Models
         }
         public void UpdateUserType(int UserID, string NewType)
         {
-            string query=$"Update [User] Set UserType=@UserType Where UserID=@UserID";
-           
+            string query = $"Update [User] Set UserType=@UserType Where UserID=@UserID";
+
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.Parameters.AddWithValue("@UserType", NewType);
             cmd.Parameters.AddWithValue("@UserID", UserID);
             try
             {
                 con.Open();
-                cmd.ExecuteNonQuery();            }
+                cmd.ExecuteNonQuery();
+            }
             catch (Exception ex)
             {
 
@@ -185,7 +188,7 @@ namespace Project.Models
         }
         public DataTable LoadCourses()
         {
-            string query = "SELECT \r\n    C.[Name] AS [Name],\r\n    Prof.[Name] AS professor_name,\r\n    TA.[Name] AS ta_name,\r\n    Jta.name AS jta_name,\r\n    COUNT(CS.Student_ID) AS Student_Count\r\nFROM Course C\r\nLEFT JOIN Professor P ON C.ProfessorID = P.ID\r\nLEFT JOIN [User] Prof ON P.ID = Prof.UserID\r\nLEFT JOIN Course_TA CT ON C.Code = CT.Course_Code\r\nLEFT JOIN TA T ON CT.TA_ID = T.ID\r\nLEFT JOIN [User] TA ON T.ID = TA.UserID\r\nLEFT JOIN JTA JT ON C.Code = JT.Course_Code\r\nLEFT JOIN Student JTASt ON JT.Student_ID = JTASt.ID\r\nLEFT JOIN [User] JTA ON JTASt.ID = JTA.UserID\r\nLEFT JOIN Course_Student CS ON C.Code = CS.Course_Code\r\nGROUP BY C.Code, C.[Name], Prof.[Name], TA.[Name], JT";
+            string query = "SELECT \r\n    C.[Name] AS [Name],\r\n    Prof.[Name] AS professor_name,\r\n    TA.[Name] AS ta_name,\r\n    JTA.[Name] AS jta_name,\r\n    COUNT(CS.Student_ID) AS Student_Count\r\nFROM Course C\r\nLEFT JOIN Professor P ON C.ProfessorID = P.ID\r\nLEFT JOIN [User] Prof ON P.ID = Prof.UserID\r\nLEFT JOIN Course_TA CT ON C.Code = CT.Course_Code\r\nLEFT JOIN TA T ON CT.TA_ID = T.ID\r\nLEFT JOIN [User] TA ON T.ID = TA.UserID\r\nLEFT JOIN JTA JT ON C.Code = JT.Course_Code\r\nLEFT JOIN Student JTASt ON JT.Student_ID = JTASt.ID\r\nLEFT JOIN [User] JTA ON JTASt.ID = JTA.UserID\r\nLEFT JOIN Course_Student CS ON C.Code = CS.Course_Code\r\nGROUP BY C.Code, C.[Name], Prof.[Name], TA.[Name], JTA.[Name]";
             DataTable dt = new DataTable();
             SqlCommand cmd = new SqlCommand(query, con);
             try
@@ -203,6 +206,167 @@ namespace Project.Models
             }
             return dt;
         }
+        public DataTable AllCoursesBriefed()
+        {
+            string query = "SELECT \r\n    C.Code AS Course_Code,\r\n    C.[Name] AS Course_Name,\r\n    U.[Name] AS Professor_Name\r\nFROM Course c\r\nLEFT JOIN [User] U ON C.ProfessorID = U.UserID";
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return dt;
+        }
+        public void AddNewCourse(string Code, string Name, int PID, string LRoom, string LDay, TimeSpan LHour, int LDuration, string TRoom, string TDay, TimeSpan THour, int TDuration)
+        {
+            string query = "INSERT INTO Course (\r\n    Code, [Name], ProfessorID,\r\n    LectureRoom, LectureDay, LectureHour, LectureDuration,\r\n    TutorialRoom, TutorialDay, TutorialHour, TutorialDuration\r\n) VALUES (\r\n    @Code, @Name, @PID,\r\n    @LRoom, @LDay, @LHour, @LDuration,\r\n    @TRoom, @TDay, @THour, @TDuration\r\n);";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Code", Code);
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@PID", PID);
+            cmd.Parameters.AddWithValue("@LRoom", LRoom);
+            cmd.Parameters.AddWithValue("@LDay", LDay);
+            cmd.Parameters.AddWithValue("@LHour", LHour);
+            cmd.Parameters.AddWithValue("@LDuration", LDuration);
+            cmd.Parameters.AddWithValue("@TRoom", TRoom);
+            cmd.Parameters.AddWithValue("@TDay", TDay);
+            cmd.Parameters.AddWithValue("@THour", THour);
+            cmd.Parameters.AddWithValue("@TDuration", TDuration);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void ADD_TA_JTA_Students(string CourseCode, List<int> TAsIDs, List<int> JTAsIDs, List<int> StudentsIDs)
+        {
+            try
+            {
+                con.Open();
+                foreach (var TAID in TAsIDs)
+                {
+                    string query = "INSERT INTO Course_TA (Course_Code, TA_ID) VALUES (@CourseCode, @TAID);\r\n";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@CourseCode", CourseCode);
+                    cmd.Parameters.AddWithValue("@TAID", TAID);
+                    cmd.ExecuteNonQuery();
+                }
+                foreach (var JTAID in JTAsIDs)
+                {
+                    string query = "INSERT INTO JTA (Course_Code, Student_ID) VALUES (@CourseCode, @JTAID);\r\n";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@CourseCode", CourseCode);
+                    cmd.Parameters.AddWithValue("@JTAID", JTAID);
+                    cmd.ExecuteNonQuery();
+                }
+                foreach (var StudentID in StudentsIDs)
+                {
+                    string query = "INSERT INTO Course_Student (Course_Code, Student_ID) VALUES (@CourseCode, @StudentID);\r\n";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@CourseCode", CourseCode);
+                    cmd.Parameters.AddWithValue("@StudentID", StudentID);
+                    cmd.ExecuteNonQuery();
+                }
+            }   
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public CourseDetailsResult GetCourseData(string CourseCode)
+        {
+            CourseDetailsResult result = new CourseDetailsResult();
+            result.TA_IDs = new List<int>();
+            result.JTA_IDs = new List<int>();
+            result.Student_IDs = new List<int>();
+
+            string FirstQuery = "SELECT \r\n    C.Code AS course_code,\r\n    C.[Name] AS course_name,\r\n    P.ProfessorID AS professor,\r\n    C.LectureRoom,\r\n    C.LectureDay,\r\n    C.LectureHour,\r\n    C.LectureDuration,\r\n    C.TutorialRoom,\t\r\n    C.TutorialDay,\r\n    C.TutorialHour,\r\n    C.TutorialDuration\r\nFROM Course C\r\nLEFT JOIN [User] P ON C.ProfessorID = P.UserID\r\nLEFT JOIN Course_TA ta ON Ta.Course_Code = C.Code\r\nLEFT JOIN JTA JTA ON JTA.Course_Code = C.Code\r\nLEFT JOIN course_Student CS ON CS.Course_code = C.Code\r\nWHERE C.Code = @CourseCode\r\nGROUP BY \r\n    C.Code, \r\n    C.[Name],\r\n    P.ProfessorID,\r\n    C.LectureRoom,\r\n    C.LectureDay,\r\n    C.LectureHour,\r\n    C.LectureDuration,\r\n    C.TutorialRoom,\r\n    C.TutorialDay,\r\n    C.TutorialHour,\r\n    C.TutorialDuration;";
+            SqlCommand cmd_1 = new SqlCommand(FirstQuery, con);
+            cmd_1.Parameters.AddWithValue("@CourseCode", CourseCode);
+
+
+            string SecondQuery = "SELECT TA_ID\r\nFROM Course_TA\r\nWHERE Course_Code = @CourseCode;";
+            SqlCommand cmd_2 = new SqlCommand(SecondQuery, con);
+            cmd_2.Parameters.AddWithValue("@CourseCode", CourseCode);
+
+            string ThirdQuery = "SELECT Student_ID\r\nFROM JTA\r\nWHERE Course_Code = @CourseCode;";
+            SqlCommand cmd_3 = new SqlCommand(ThirdQuery, con);
+            cmd_3.Parameters.AddWithValue("@CourseCode", CourseCode);
+
+            string FourthQuery = "SELECT Student_ID\r\nFROM Course_Student\r\nWHERE Course_Code = @CourseCode;";
+            SqlCommand cmd_4 = new SqlCommand(FourthQuery, con);
+            cmd_4.Parameters.AddWithValue("@CourseCode", CourseCode);
+
+            try
+            {
+                con.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd_1);
+                DataTable courseInfo = new DataTable();
+                adapter.Fill(courseInfo);
+                result.CourseInfo = courseInfo;
+
+                SqlDataReader reader = cmd_2.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.TA_IDs.Add(reader.GetInt32(0));
+                }
+                reader.Close();
+
+                reader = cmd_3.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.JTA_IDs.Add(reader.GetInt32(0));
+                }
+                reader.Close();
+
+                reader = cmd_4.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Student_IDs.Add(reader.GetInt32(0));
+                }
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return result;
+        }
+    }
+
+    public class CourseDetailsResult
+    {
+        public DataTable CourseInfo { get; set; }
+        public List<int> TA_IDs { get; set; }
+        public List<int> JTA_IDs { get; set; }
+        public List<int> Student_IDs { get; set; }
 
     }
 }
--- WebCode/Project/Pages/Registrar/AddCourse.cshtml.cs
+++ WebCode/Project/Pages/Registrar/AddCourse.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project.Models;
@@ -6,6 +7,10 @@ namespace Project.Pages.Registrar
 {
     public class AddCourseModel : PageModel
     {
+        public string TA_IDs_CSV { get; set; }
+        public string JTA_IDs_CSV { get; set; }
+        public string Student_IDs_CSV { get; set; }
+        public DataTable DT { get; set; } = new DataTable();
         public DB db { get; set; }
         public AddCourseModel(DB db)
         {
@@ -22,6 +27,7 @@ namespace Project.Pages.Registrar
             }
             else
             {
+                DT = db.AllCoursesBriefed();
                 return Page();
             }
         }
@@ -32,15 +38,46 @@ namespace Project.Pages.Registrar
             {
                 return Page();
             }
+            db.AddNewCourse(Input.CourseCode, Input.CourseName, Input.Professor, Input.LectureRoom, Input.LectureDay, TimeSpan.Parse(Input.LectureHour), Input.LectureDuration ?? 0, Input.TutorialRoom, Input.TutorialDay, TimeSpan.Parse(Input.TutorialHour), Input.TutorialDuration ?? 0);
+            db.ADD_TA_JTA_Students(Input.CourseCode, ParseIds(Input.TAsIDs), ParseIds(Input.JTAsIDs), ParseIds(Input.StudentsIDs));
 
             return RedirectToPage();
+        }
+        public IActionResult OnPostEditCourse(string Course_Code)
+        {
+            var result= db.GetCourseData(Course_Code);
+            if (result?.CourseInfo == null || result.CourseInfo.Rows.Count == 0)
+            {
+                // Show error or redirect as needed
+                ModelState.AddModelError(string.Empty, "Course data not found.");
+                return Page();
+            }
+            Input = new CourseInputModel
+            {
+                CourseCode = result.CourseInfo.Rows[0]["course_code"].ToString(),
+                CourseName = result.CourseInfo.Rows[0]["course_name"].ToString(),
+                Professor = Convert.ToInt32(result.CourseInfo.Rows[0]["professor"]),
+                LectureRoom = result.CourseInfo.Rows[0]["LectureRoom"].ToString(),
+                LectureDay = result.CourseInfo.Rows[0]["LectureDay"].ToString(),
+                LectureHour = result.CourseInfo.Rows[0]["LectureHour"].ToString(), // Keep as string
+                LectureDuration = Convert.ToInt32(result.CourseInfo.Rows[0]["LectureDuration"]),
+                TutorialRoom = result.CourseInfo.Rows[0]["TutorialRoom"].ToString(),
+                TutorialDay = result.CourseInfo.Rows[0]["TutorialDay"].ToString(),
+                TutorialHour = result.CourseInfo.Rows[0]["TutorialHour"].ToString(), // Keep as string
+                TutorialDuration = Convert.ToInt32(result.CourseInfo.Rows[0]["TutorialDuration"]),
+                TAsIDs = string.Join(",", result.TA_IDs),
+                JTAsIDs = string.Join(",", result.JTA_IDs),
+                StudentsIDs = string.Join(",", result.Student_IDs)
+            };
+
+            return Page();
         }
 
         public class CourseInputModel
         {
             public string CourseCode { get; set; }
             public string CourseName { get; set; }
-            public string Professor { get; set; }
+            public int Professor { get; set; }
             public string TAsIDs { get; set; }
             public string JTAsIDs { get; set; }
             public string StudentsIDs { get; set; }
@@ -52,6 +89,19 @@ namespace Project.Pages.Registrar
             public string TutorialDay { get; set; }
             public string TutorialHour { get; set; }
             public int? TutorialDuration { get; set; }
+        }
+        private List<int> ParseIds(string idString)
+        {
+            if (string.IsNullOrWhiteSpace(idString))
+                return new List<int>();
+
+            return idString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(id => id.Trim())
+                          .Where(id => !string.IsNullOrEmpty(id))
+                          .Select(id => int.TryParse(id, out var num) ? num : (int?)null)
+                          .Where(id => id.HasValue)
+                          .Select(id => id.Value)
+                          .ToList();
         }
     }
 }
