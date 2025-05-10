314d83096639881651190813f2f82b2f9032b03a
Hassan Rashwan <163678921+Ahito498@users.noreply.github.com>
2025-05-10 08:48:53 +0300

    updated

--- WebCode/Project/Pages/CleaningStaff/DailyCleaning.cshtml.cs
+++ WebCode/Project/Pages/CleaningStaff/DailyCleaning.cshtml.cs
@@ -1,26 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project.Models;
+using System.Data;
 
 namespace Project.Pages.CleaningStaff
 {
     public class Daily_CleaningModel : PageModel
     {
         public DB db { get; set; }
-        public Daily_CleaningModel(DB db)
+
+        public DataTable DailyStatuses { get; set; } = new DataTable();
+
+        [BindProperty]
+        public string SelectedRoom { get; set; }
+
+        [BindProperty]
+        public string SelectedCondition { get; set; }
+        
+      
+        public DataTable RoomCleaningTable { get; set; }
+
+        
+
+        public Daily_CleaningModel()
         {
-            this.db = db;
+            db = new DB(); // Manual instance for now
         }
+
         public IActionResult OnGet()
         {
+            // Check if user is logged in
             if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserType")))
             {
+                // If not logged in, redirect to login page
                 return RedirectToPage("/Login");
             }
-            else
+            
+            DailyStatuses = db.LoadDailyCleaningStatuses();
+            return Page();
+            
+        }
+
+        public IActionResult OnPost()
+        {   
+            
+            if (!string.IsNullOrEmpty(SelectedRoom) && !string.IsNullOrEmpty(SelectedCondition))
             {
-                return Page();
+                db.UpdateRoomCleaningStatus(SelectedRoom, SelectedCondition);
             }
+
+            DailyStatuses = db.LoadDailyCleaningStatuses();
+            return Page();
         }
     }
 }
