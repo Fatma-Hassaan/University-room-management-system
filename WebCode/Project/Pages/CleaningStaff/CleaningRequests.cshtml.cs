ff3334be664271d2cc44a523901191ef366b7476
Hassan Rashwan <163678921+Ahito498@users.noreply.github.com>
2025-05-10 08:47:46 +0300

    Cleaning Requests update

--- WebCode/Project/Pages/CleaningStaff/CleaningRequests.cshtml.cs
+++ WebCode/Project/Pages/CleaningStaff/CleaningRequests.cshtml.cs
@@ -1,26 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project.Models;
+using System.Data;
 
 namespace Project.Pages.CleaningStaff
 {
     public class Cleaning_RequestsModel : PageModel
     {
         public DB db { get; set; }
-        public Cleaning_RequestsModel(DB db)
+        public DataTable CleaningRequestsTable { get; set; }
+
+        public Cleaning_RequestsModel()
         {
-            this.db = db;
+            db = new DB(); // Manually create instance since not using DI
         }
+
         public IActionResult OnGet()
         {
+            // Check if user is logged in
             if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserType")))
             {
+                // If not logged in, redirect to login page
                 return RedirectToPage("/Login");
             }
-            else
-            {
-                return Page();
-            }
+
+            // Otherwise, load the cleaning requests (real or mock)
+            CleaningRequestsTable = db.LoadCleaningRequests();
+
+            return Page();
         }
+
+
+        public IActionResult OnPostMarkAsDone(int requestId)
+        {
+            db.MarkCleaningRequestAsHandled(requestId);
+            return RedirectToPage(); // Refresh the list after marking
+        }
+
+        
+        
+        
     }
 }
+
