753e47341ef496632133a4f9cc96cb672831b88b
Hassan Rashwan <163678921+Ahito498@users.noreply.github.com>
2025-05-13 09:03:19 +0300

    Add files via upload

--- WebCode/Project/Pages/RoomServicesTeam/CleaningRequests.cshtml.cs
+++ WebCode/Project/Pages/RoomServicesTeam/CleaningRequests.cshtml.cs
@@ -1,50 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Http;
-using System.Data;
 using Project.Models;
+using System.Data;
 
 namespace Project.Pages.RoomServicesTeam
 {
-    public class CleaningRequestsModel : PageModel
+    public class Cleaning_RequestModel : PageModel
     {
-        public DB db { get; set; }
-        public DataTable RequestsTable { get; set; }
-        public List<string> AvailableRooms { get; set; }
+        private readonly DB db;
 
-        public CleaningRequestsModel(DB db)
+        [BindProperty]
+        public string SelectedRoomId { get; set; }
+
+        public List<string> RoomsList { get; set; } = new List<string>();
+        public DataTable CleaningRequestsTable { get; set; } = new DataTable();
+
+        public int CurrentUserId => 
+            string.IsNullOrEmpty(HttpContext.Session.GetString("Email")) 
+                ? 0 
+              : db.GetUserID(HttpContext.Session.GetString("Email"));
+
+        public Cleaning_RequestModel(DB db)
         {
             this.db = db;
         }
 
         public IActionResult OnGet()
         {
-            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserType")))
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserType")) ||
+                HttpContext.Session.GetString("UserType") != "RoomServicesMember")
             {
                 return RedirectToPage("/Login");
             }
 
-            LoadPageData();
+            RoomsList = db.GetAvailableRoomIDs();
+            CleaningRequestsTable = db.LoadAllCleaningRequestsForRoomServices();
+
             return Page();
         }
+       
 
-        public IActionResult OnPostNewRequest(string RoomID)
-        {
-            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserType")))
-            {
-                return RedirectToPage("/Login");
-            }
+       public IActionResult OnPostSubmitCleaningRequest()
+       {
+           
+           Console.WriteLine("Selected Room: " + SelectedRoomId);
+           Console.WriteLine("Current User ID: " + CurrentUserId);
 
-            int userId = db.GetUserID(HttpContext.Session.GetString("Email")); // Assuming Email is stored
-            db.InsertCleaningRequest(userId, RoomID);
+           if (!string.IsNullOrEmpty(SelectedRoomId) && CurrentUserId != 0)
+           {
+               db.InsertCleaningRequest(CurrentUserId, SelectedRoomId);
+               Console.WriteLine("InsertCleaningRequest called successfully.");
+           }
+           else
+           {
+               Console.WriteLine("Request NOT inserted. Either room ID is missing or user ID is 0.");
+           }
 
-            return RedirectToPage(); // Refresh the page
-        }
+           return RedirectToPage(); // Reload and reflect updated data
+       }
 
-        private void LoadPageData()
-        {
-            RequestsTable = db.LoadAllCleaningRequests(); 
-            AvailableRooms = db.GetAvailableRoomIDs();    
-        }
     }
 }
